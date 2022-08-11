024fe3278ebcf6c60e826100d4ba1a2f803619c4
Simon Semakov <simon.semakov@gmail.com>
2022-08-11 02:14:27 +0000

    Update the rest of the main banner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Divider, Grid, Typography } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { Box, Container } from "@mui/system";
@@ -13,23 +13,39 @@ const App = () => (
       <Box
         sx={{
           backgroundColor: "#2625ec47",
-          // backgroundImage: "linear-gradient(135deg, #0f2f79, #3f51b529)",
           height: "100vh",
         }}
       >
         <Grid container>
           <Grid item sm={8}>
             <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-              <GradientTypography variant="h1">
-                Hello
-                <br />
-                My name is Simon
-              </GradientTypography>
-              <hr />
-              <p>
-                I do many diffrent things. You can find about some of them on
-                this site.
-              </p>
+              <Grid container direction="column" spacing={2}>
+                <Grid item>
+                  <GradientTypography variant="h1">
+                    Hello
+                    <br />
+                    My name is Simon
+                  </GradientTypography>
+                </Grid>
+                <Grid item>
+                  <Grid container>
+                    <Grid item sm={1}>
+                      <Divider
+                        sx={{
+                          borderBottomWidth: "thick",
+                          borderColor: "#818bc2",
+                        }}
+                      />
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h4" sx={{ color: "#272f5f" }}>
+                    I do many diffrent things. You can find about some of them
+                    on this site.
+                  </Typography>
+                </Grid>
+              </Grid>
             </Container>
           </Grid>
           <Grid item sm={4}>
