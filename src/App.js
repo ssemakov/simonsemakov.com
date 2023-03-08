c424e512ad82b70796f514f978f95eb08fd68f7b
Simon Semakov <simon.semakov@gmail.com>
2023-03-08 10:50:16 -0500

    Remove the scrollbar on mobile WebKit screens.  (#6)
    * Update MUI to 5.11.12
    
    * Fix full screen size for Mobile WebKit browsers (avoid scrollbar).
    
    * Add postcss and 100vh-fix
    
    * Remove App.css imports
    
    * Fix the height for webkit and chrome

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import { Grid } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
-import { Box } from "@mui/system";
-import "./App.css";
 import HomePageContent from "./HomePageContent";
 import Sidebar from "./Sidebar";
 import theme from "./theme";
 
 const Layout = ({ leftPaine: LeftPaine, rightPaine: RightPaine }) => (
-  <Box
-    sx={{
-      height: "100vh",
-    }}
-  >
-    <Grid container>
-      <Grid item md={8}>
-        <LeftPaine />
-      </Grid>
-      <Grid item md={4}>
-        <RightPaine />
-      </Grid>
+  <Grid container>
+    <Grid item md={8}>
+      <LeftPaine />
     </Grid>
-  </Box>
+    <Grid item md={4}>
+      <RightPaine />
+    </Grid>
+  </Grid>
 );
 
 const App = () => (
--- src/HomePageContent.js
+++ src/HomePageContent.js
@@ -3,7 +3,7 @@ import { Box } from "@mui/system";
 import GradientTypography from "./GradientTypography";
 
 const HomePageContent = () => (
-  <Box sx={{ height: "100vh" }} display="flex">
+  <Box className="full-height" display="flex">
     <Box m="auto">
       <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="lg">
         <Grid container direction="column" spacing={2}>
