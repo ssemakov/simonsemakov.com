5e3ebd1636f022225d5024d5099765a768651a94
Simon Semakov <simon.semakov@gmail.com>
2023-03-09 12:48:14 -0500

    Add next.js app skeleton using appDir.

---
+++ app/home-page.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Grid } from "@mui/material";
+// import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+// import HomePageContent from "./HomePageContent";
+// import Sidebar from "./Sidebar";
+// import theme from "./theme";
+
+const HomePageLayout = ({ leftPaine: LeftPaine, rightPaine: RightPaine }) => (
+  <Grid container>
+    <Grid item md={8}>
+      <LeftPaine />
+    </Grid>
+    <Grid item md={4}>
+      <RightPaine />
+    </Grid>
+  </Grid>
+);
+
+const HomePageContent = () => <div>Home Page Content</div>;
+const Sidebar = () => <div>Sidebar</div>;
+const theme = {};
+
+export default function HomePage() {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        {/* <CssBaseline enableColorScheme /> */}
+        <HomePageLayout leftPaine={HomePageContent} rightPaine={Sidebar} />
+      </ThemeProvider>
+    </>
+  );
+}
