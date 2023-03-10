9d7b91fd7b3b573469e58db6a640ce756fe32338
Simon Semakov <simon.semakov@gmail.com>
2023-03-10 11:53:54 -0500

    Imports order.

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
-import React from "react";
 import Head from "next/head";
 import Script from "next/script";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
+import React from "react";
+
 import { CacheProvider } from "@emotion/react";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+
 import DefaultLayout from "../components/DefaultLayout";
-import theme from "../lib/theme";
 import createEmotionCache from "../lib/emotionCache";
-import styles from "../styles/globals.css";
+import theme from "../lib/theme";
+
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import styles from "../styles/globals.css";
 
 config.autoAddCss = false;
 
