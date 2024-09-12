4886bd15a8cac829a056979fbd36e0321b30518a
Simon Semakov <ssemakov@wistia.com>
2024-09-11 21:34:15 -0400

    Dynamic resize for the photo galery

--- pages/photos/istanbul.tsx
+++ pages/photos/istanbul.tsx
@@ -1,29 +1,41 @@
 import { Box, Container } from "@mui/material";
 import Grid from "@mui/material/Grid2";
+import { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
 import Sidebar from "../../components/Sidebar";
 
-const Istanbul = () => (
-  <div style={{ height: "1500px" }}>
-    <iframe
-      src="https://www.playbook.com/e/simons83/s6arErSms7dmM6w5VdrznbWa?theme=gallery"
-      title="Istanbul"
-      sandbox="allow-same-origin allow-scripts"
-      frameBorder="0"
-      width="100%"
-      height="100%"
-    ></iframe>
-  </div>
-);
+const Istanbul = () => {
+  const [windowHeight, setWindowHeight] = useState(650);
+  const handleResize = () => {
+    setWindowHeight(window.innerHeight - 100);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize, false);
+  }, []);
+
+  return (
+    <Box sx={{ height: `${windowHeight}px` }}>
+      <iframe
+        src="https://www.playbook.com/e/simons83/s6arErSms7dmM6w5VdrznbWa?theme=gallery"
+        title="Istanbul"
+        sandbox="allow-same-origin allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      ></iframe>
+    </Box>
+  );
+};
 
 const PhotoAlbumContainer = () => (
-  <Container fixed maxWidth="xl">
-    <Box sx={{ m: 8 }} />
-    <Grid container spacing={2} columns={12}>
-      <Grid size={{ sm: 12, md: 8, lg: 10 }}>
-        <Istanbul />
+  <Container maxWidth="xl" sx={{ height: "100%" }}>
+    <Box sx={{ m: 8, justifySelf: "center" }}>
+      <Grid container>
+        <Grid size={{ sm: 12, md: 8, lg: 10 }}>
+          <Istanbul />
+        </Grid>
       </Grid>
-    </Grid>
+    </Box>
   </Container>
 );
 
